2a40d565401cebd4f5c7f474086c67f0f4eb9bbf
vyshnavi20-gunda <vyshnavigupta20@gmail.com>
2026-04-10 18:51:33 +0530

    first commit

--- src/components/TypingBox.jsx
+++ src/components/TypingBox.jsx
@@ -1,12 +1,12 @@
-const keys = ['a', 's', 'd', 'f', 'j', 'k', 'l', ';'];
-
-export const generateSequence = (length = 200) => {
-    let sequence = [];
-
-    for (let i = 0; i < length; i++) {
-        const randomKey = keys[Math.floor(Math.random() * keys.length)];
-        sequence.push(randomKey);
-    }
-
-    return sequence;
+const TypingBox = ({ handleKeyPress }) => {
+    return (
+        <input
+            type="text"
+            autoFocus
+            onKeyDown={handleKeyPress}
+            className="typing-box"
+        />
+    );
 };
+
+export default TypingBox; 
